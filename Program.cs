822b2828d3138be92a83c1b557dfc766091c67f6
lucas roque novaes <lucasroque0802@gmail.com>
2026-04-24 22:07:40 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿double tamanhoBytes, velocidadeMbps, tempoMin;
+
+
+Console.WriteLine(" TEMPO DE DOWNLOAD \n");
+
+
+Console.Write("Tamanho do arquivo em MB........: ");
+
+tamanhoBytes = Convert.ToDouble(Console.ReadLine());
+
+
+Console.Write("Velocidade da conexão em Mbps...: ");
+
+velocidadeMbps = Convert.ToDouble(Console.ReadLine());
+
+
+tempoMin = tamanhoBytes * 8 / velocidadeMbps / 60;
+
+
+Console.WriteLine($"\nTempo estimado de download: {tempoMin:N1} minutos");
